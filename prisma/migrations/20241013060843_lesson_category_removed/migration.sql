1d6babf79e8223793b68e07f080f507ed989d751
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-13 13:45:26 +0500

    feat: lesson's category removed migrated

---
+++ prisma/migrations/20241013060843_lesson_category_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `category` on the `Lesson` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Lesson" DROP COLUMN "category";
